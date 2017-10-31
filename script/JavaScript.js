7b9655b09a525607934ae9d0980b97cd2962bf67
Wojciech <wojciech.najda.sg@gmail.com>
2017-10-31 12:02:43 +0100

    I corrected the button. I think button now works better.

--- script/JavaScript.js
+++ script/JavaScript.js
@@ -5,4 +5,17 @@ $(document).ready(function()
     weatherWidget.weatherWidgetObject = $('#containerForweatherWidget');
     weatherWidget.viewWeatherWidget();
 })
+$(window).scroll(()=>{
+  if($(document).scrollTop() >1400){
+    $('.return-button-to-start_hide').removeClass().addClass('return-button-to-start');
+  }else{
+    $('.return-button-to-start').removeClass().addClass('return-button-to-start_hide');
+  }
+})
+$("#return-button").on("click",function(){
+  $("body,html").animate({ scrollTop: 0 }, 1200);
+})
+
+
+
 
