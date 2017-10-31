11a69e4c4bc1ffa9d43398d2ef0abfd69f0020e8
Wojciech <wojciech.najda.sg@gmail.com>
2017-10-31 19:20:57 +0100

    I did a function that shows the current position on the web page.

--- script/JavaScript.js
+++ script/JavaScript.js
@@ -1,21 +1,57 @@
-
-$(document).ready(function()
-                 {
-    weatherWidget.defaultImage = "img/dribbbbleeeeee.jpg"
-    weatherWidget.weatherWidgetObject = $('#containerForweatherWidget');
-    weatherWidget.viewWeatherWidget();
+$(document).ready(() => {
+  weatherWidget.defaultImage = "img/dribbbbleeeeee.jpg"
+  weatherWidget.weatherWidgetObject = $('#containerForweatherWidget');
+  weatherWidget.viewWeatherWidget();
 })
-$(window).scroll(()=>{
-  if($(document).scrollTop() >1400){
+
+$(window).scroll(() => {
+  if ($(document).scrollTop() > 1400) {
     $('.return-button-to-start_hide').removeClass().addClass('return-button-to-start');
-  }else{
+  } else {
     $('.return-button-to-start').removeClass().addClass('return-button-to-start_hide');
   }
 })
-$("#return-button").on("click",function(){
-  $("body,html").animate({ scrollTop: 0 }, 1200);
+
+$("#return-button").on("click", () => {
+  $("body,html").animate({
+    scrollTop: 0
+  }, 1200);
 })
 
+$(window).scroll(() => {
+  let firstA = $('#firtsAInNav');
+  let secondA = $('#secondAInNav');
+  let thirdA = $("#thirdAInNav");
+  let forthA = $("#fourthAInNav");
+  let scrollTop = $(document).scrollTop();
+  let heroHeaderOffsetTop = $('#hero-header')["0"].offsetTop;
+  let aboutAppOffsetTop = $('#about-app')["0"].offsetTop;
+  let descriptionOffsetTop = $('#description')["0"].offsetTop;
+  let teamOffsetTop = $('#team')["0"].offsetTop;
+  let newsletterOffsetTop = $('#newsletter')["0"].offsetTop;
+  let footerOffsetTop = $('#footer')["0"].offsetTop;
 
+  if (scrollTop > aboutAppOffsetTop - aboutAppOffsetTop / 2 && scrollTop < descriptionOffsetTop - descriptionOffsetTop / 5) {
+    firstA.removeClass().addClass('position-on-page');
+  } else {
+    firstA.removeClass().addClass('link-menu');
+  }
 
+  if (scrollTop > descriptionOffsetTop - descriptionOffsetTop / 5 && scrollTop < teamOffsetTop - teamOffsetTop / 5) {
+    secondA.removeClass().addClass('position-on-page');
+  } else {
+    secondA.removeClass().addClass('link-menu');
+  }
 
+  if (scrollTop > teamOffsetTop - teamOffsetTop / 5 && scrollTop < newsletterOffsetTop - newsletterOffsetTop / 10) {
+    thirdA.removeClass().addClass('position-on-page');
+  } else {
+    thirdA.removeClass().addClass('link-menu');
+  }
+
+  if (scrollTop > newsletterOffsetTop - newsletterOffsetTop / 10 && scrollTop < footerOffsetTop - footerOffsetTop / 10) {
+    forthA.removeClass().addClass('position-on-page');
+  } else {
+    forthA.removeClass().addClass('link-menu');
+  }
+})
