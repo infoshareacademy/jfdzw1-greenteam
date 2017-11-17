43e1edb430c4bc72e69655cdb5e8f4f6668cff8c
achroscielewska <achroscielewska@gmail.com>
2017-11-17 21:28:38 +0100

    JFDZW1TG-48 | funkcja initScoreboard - sprawdzam czy scoreboard jest pusty

--- game/js/scoreboard.js
+++ game/js/scoreboard.js
@@ -1,8 +1,18 @@
 var scoreboard = [];
 
-function initScoreboard() {
-
+function initScoreboard(scoreboard) {
+    //sprawdzi czy tablica scoreboard jest pusta
+    //jeżeli jest pusta, to:
+    //utworzy pięciu inicjalnych playerów (przez funkcję createPlayer; różne imiona i różne wyniki - 1000, 2000, 3000, 4000, 5000)
+    //wstawi ich za pomocą funkcji insertPlayer
+    // check if scoreboard is empty
+    var scoreboardLn = scoreboard.length;
+    if (scoreboardLn === 0) {
+        return document.getElementById("initScoreboardTest").innerHTML=true;
+    }
 }
+
+initScoreboard(scoreboard);
 
 function createPlayer() {
 
