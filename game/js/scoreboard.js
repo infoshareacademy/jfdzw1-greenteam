29692ef15a46aba1749dd1d5774bc5aecbe7e05e
achroscielewska <achroscielewska@gmail.com>
2017-11-17 21:58:40 +0100

    JFDZW1TG-48 | funkcja initScoreboard - tworzę inicjalnego playera i wstawiam go do tabeli

--- game/js/scoreboard.js
+++ game/js/scoreboard.js
@@ -1,14 +1,19 @@
 var scoreboard = [];
 
 function initScoreboard(scoreboard) {
-    //sprawdzi czy tablica scoreboard jest pusta
-    //jeżeli jest pusta, to:
-    //utworzy pięciu inicjalnych playerów (przez funkcję createPlayer; różne imiona i różne wyniki - 1000, 2000, 3000, 4000, 5000)
-    //wstawi ich za pomocą funkcji insertPlayer
-    // check if scoreboard is empty
+    // check if scoreboard is empty, if empty insert five initial players using insertPlayer function
     var scoreboardLn = scoreboard.length;
     if (scoreboardLn === 0) {
-        return document.getElementById("initScoreboardTest").innerHTML=true;
+        var i = 1;
+        var initPlayerName;
+        var initPlayerScore;
+        while (i < 6) {
+            initPlayerName = "Jan" + i;
+            initPlayerScore = i*1000;
+            createPlayer(initPlayerName,initPlayerScore);
+            insertPlayer(initPlayerName,initPlayerScore);
+            i++;
+        }
     }
 }
 
