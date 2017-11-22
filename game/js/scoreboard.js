11096eaa4f686b33c784c0abeedc602f54d895ac
achroscielewska <achroscielewska@gmail.com>
2017-11-22 19:33:45 +0100

    JFDZW1TG-56 | poprawka w js funkcja initScoreboard zmiana var na let

--- game/js/scoreboard.js
+++ game/js/scoreboard.js
@@ -26,13 +26,11 @@ let gameboard =
 
 function initScoreboard (scoreboard) {
     // check if scoreboard is empty, if empty insert five initial players using insertPlayer function
-    var scoreboardLn = scoreboard.length;
+    let scoreboardLn = scoreboard.length;
     if (scoreboardLn === 0) {
-        var i = 1;
-        var initPlayerName;
-        var initPlayerScore;
-        var initPlayer;
-
+        let initPlayerName, initPlayerScore, initPlayer;
+        let i = 1;
+        
         while (i < 6) {
             initPlayerName = "Jan" + i;
             initPlayerScore = i * 1000;
