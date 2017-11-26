3ce16914fea8a44e595769eaf3afaad84447523b
wojtexnaj <wojciech.najda.sg@gmail.com>
2017-11-26 11:09:01 +0100

    JFDZW1TG-59 | little change

--- script/JavaScript.js
+++ script/JavaScript.js
@@ -1,3 +1,18 @@
+let firstA = $('#firtsAInNav');
+let secondA = $('#secondAInNav');
+let thirdA = $("#thirdAInNav");
+let forthA = $("#fourthAInNav");
+let scrollTop = $(document).scrollTop();
+let heroHeaderOffsetTop = $('#hero-header')["0"].offsetTop;
+let aboutAppOffsetTop = $('#about-app')["0"].offsetTop;
+let descriptionOffsetTop = $('#description')["0"].offsetTop;
+let teamOffsetTop = $('#team')["0"].offsetTop;
+let newsletterOffsetTop = $('#newsletter')["0"].offsetTop;
+let footerOffsetTop = $('#footer')["0"].offsetTop;
+let portfoliosHide = $('.portfolio');
+
+
+
 $(document).ready(() => {
   weatherWidget.defaultImage = "img/dribbbbleeeeee.jpg"
   weatherWidget.weatherWidgetObject = $('#containerForweatherWidget');
@@ -19,18 +34,18 @@ $("#return-button").on("click", () => {
 })
 
 $(window).scroll(() => {
-  let firstA = $('#firtsAInNav');
-  let secondA = $('#secondAInNav');
-  let thirdA = $("#thirdAInNav");
-  let forthA = $("#fourthAInNav");
-  let scrollTop = $(document).scrollTop();
-  let heroHeaderOffsetTop = $('#hero-header')["0"].offsetTop;
-  let aboutAppOffsetTop = $('#about-app')["0"].offsetTop;
-  let descriptionOffsetTop = $('#description')["0"].offsetTop;
-  let teamOffsetTop = $('#team')["0"].offsetTop;
-  let newsletterOffsetTop = $('#newsletter')["0"].offsetTop;
-  let footerOffsetTop = $('#footer')["0"].offsetTop;
-  let portfoliosHide = $('.portfolio');
+  firstA = $('#firtsAInNav');
+  secondA = $('#secondAInNav');
+  thirdA = $("#thirdAInNav");
+  forthA = $("#fourthAInNav");
+  scrollTop = $(document).scrollTop();
+  heroHeaderOffsetTop = $('#hero-header')["0"].offsetTop;
+  aboutAppOffsetTop = $('#about-app')["0"].offsetTop;
+  descriptionOffsetTop = $('#description')["0"].offsetTop;
+  teamOffsetTop = $('#team')["0"].offsetTop;
+  newsletterOffsetTop = $('#newsletter')["0"].offsetTop;
+  footerOffsetTop = $('#footer')["0"].offsetTop;
+  portfoliosHide = $('.portfolio');
 
   if (scrollTop > aboutAppOffsetTop - aboutAppOffsetTop / 2 && scrollTop < descriptionOffsetTop - descriptionOffsetTop / 5) {
     firstA.removeClass().addClass('position-on-page');
@@ -57,13 +72,20 @@ $(window).scroll(() => {
   }
   
   
-  
+  test(scrollTop, teamOffsetTop,newsletterOffsetTop, portfoliosHide);
+
+})
+
+function test (scrollTop, teamOffsetTop,newsletterOffsetTop, portfoliosHide) {
+
+
+
   if(scrollTop > teamOffsetTop - teamOffsetTop / 5 && scrollTop < newsletterOffsetTop - newsletterOffsetTop / 8){
     portfoliosHide.show( "fold", 2000 );
   }else{
     portfoliosHide.hide( "size", { direction: "up" }, "slow" );
   }
-})
+}
 
 
   
