74ce82f81d40863572f2bf94d8de65b095f2a794
achroscielewska <achroscielewska@gmail.com>
2017-12-04 17:40:15 +0100

    JFDZW1TG-56 | pobranie name z sekcji newsletter i przekazanie do modala Game

--- game/js/game.js
+++ game/js/game.js
@@ -6,3 +6,21 @@ $ (document).ready (function () {
 }
 });
 
+var $playerName = '';
+
+$(function () {
+    $('button').click(function (e) {
+        e.preventDefault();
+
+        $playerName = ($('#customerName').val());
+
+        console.log($playerName);
+
+    });
+});
+
+
+$('#modalGame').on('shown.bs.modal', function (e) {
+    alert($playerName);
+})
+
