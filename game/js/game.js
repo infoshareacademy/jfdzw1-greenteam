d15f1f4d1cf1de44168cfcc6531b779d2bd81cfb
achroscielewska <achroscielewska@gmail.com>
2017-12-05 19:34:58 +0100

    JFDZW1TG-56 | przekazanie imienia z newsletter na okienko modalne

--- game/js/game.js
+++ game/js/game.js
@@ -5,6 +5,25 @@ $ (document).ready (function () {
 
     }
 
+    // get player name form scr
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    var $playerName = getParameterByName('name');
+    alert($playerName);
+
+    let $object = $('#currentPlayer').empty();
+
+    $(`<b>${$playerName}</b>`)
+        .prependTo('#currentPlayer');
+
 });
 
 
